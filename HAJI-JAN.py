0e124f6fa17d19e3b1719ce846ceaae4e26a07dc
Haji Jan <93093275+SawashRahi@users.noreply.github.com>
2023-11-05 08:07:04 +0430

    HAJI-JAN.py

--- HAJI-JAN.py
+++ HAJI-JAN.py
@@ -2,5 +2,8 @@ import os,platform
 os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('pip uninstall requests')
+os.system ('pip install requests')
 os.system('xdg-open https://t.me/Afg_Black001')
-import rah12
+import t12
